93871f07a123626b4c3f5e6ae2cd712dec37e252
RDTaguines <rachelyntaguines@gmail.com>
2022-07-02 04:15:52 +0800

    modify files commit by rachelyn

--- controller/editRecordsController.js
+++ controller/editRecordsController.js
@@ -16,7 +16,7 @@ const edit_view = (req, res) => {
 //updete records
 const update_records = (req, res) => {
     const id = req.params.id;
-    let sql = "UPDATE tblstudents SET first_name='"+req.body.first_name+"', last_name='"+req.body.last_name+"', address='"+req.body.address+"', gender='"+req.body.gender+"', dob='"+req.body.dob+"', email='"+req.body.email+"', status='"+req.body.status+"', average='"+req.body.average+"', subject1='"+req.body.subject1+"', subject2='"+req.body.subject2+"', subject3='"+req.body.subject3+"', image='"+req.body.image_name+"' WHERE studentID = "+id;
+    let sql = "UPDATE tblstudents SET first_name='"+req.body.first_name+"', last_name='"+req.body.last_name+"', address='"+req.body.address+"', gender='"+req.body.gender+"', dob='"+req.body.dob+"', email='"+req.body.email+"', status='"+req.body.status+"', average='"+req.body.average+"', subject1='"+req.body.subject1+"', subject2='"+req.body.subject2+"', subject3='"+req.body.subject3+"' WHERE studentID = "+id;
     let query = newConnection.query(sql,(err, results)=>{
         if(err) throw err;
         res.render('editStudent', {
